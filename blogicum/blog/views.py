fbb914500ecf3784d425274f0b4d7a92dcc73d26
Лев Хорошилов <levyahorosh@yandex.ru>
2025-03-31 23:04:41 +0300

    cxcx

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -67,7 +67,8 @@ class UserPageListView(ListView):
 
         return posts.filter(
             author__username=username
-            ).select_related('author').annotate(comment_count=Count('comment')
+        ).select_related('author').annotate(
+            comment_count=Count('comment')
         ).order_by('-pub_date')
 
     def get_context_data(self, **kwargs):
