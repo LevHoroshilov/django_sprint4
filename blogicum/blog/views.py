ad2c2e068920e2605a73282f61b9930550050cf8
Лев Хорошилов <levyahorosh@yandex.ru>
2025-03-31 22:55:06 +0300

    sssssasasas

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -66,8 +66,8 @@ class UserPageListView(ListView):
             posts = Post.objects
 
         return posts.filter(author__username=username
-                ).select_related('author').annotate(
-                comment_count=Count('comment')
+            ).select_related('author').annotate(
+            comment_count=Count('comment')
         ).order_by('-pub_date')
 
     def get_context_data(self, **kwargs):
